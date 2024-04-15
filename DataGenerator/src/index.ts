4492f1adbf3d1f557234c7717357573867c2b282
Daiana Aysa <da.aysa@gmail.com>
2024-04-15 09:58:42 -0300

    casos de test

--- DataGenerator/src/index.ts
+++ DataGenerator/src/index.ts
@@ -2,15 +2,98 @@ import axios from 'axios';
 import { faker } from '@faker-js/faker';
 import { Person } from './Interfaces/person';
 
-const sendPatient = async () => {
-    const patient: Person = {
-      nombre: faker.person.firstName(),
-      apellido: faker.person.lastName(),
-      cedula: faker.helpers.rangeToNumber({ min: 10000000, max: 99999999 }),
-      telefono: faker.phone.number('09# ### ###'),
-      departamento: faker.location.state(),
-      necesita_asistencia_movilidad: faker.datatype.boolean({ probability: 0.3 }),
-    };
+const testPatients: Person[] = [
+  {
+    nombre: faker.person.firstName(),
+    apellido: faker.person.lastName(),
+    cedula: faker.helpers.rangeToNumber({ min: 10000000, max: 99999999 }),
+    telefono: faker.phone.number('09# ### ###'),
+    departamento: faker.location.state(),
+    necesita_asistencia_movilidad: faker.datatype.boolean({ probability: 0.3 }),
+  },
+  {
+    nombre: faker.person.firstName(),
+    apellido: faker.person.lastName(),
+    cedula: faker.helpers.rangeToNumber({ min: 10000000, max: 99999999 }),
+    telefono: faker.phone.number('09#######'),
+    departamento: faker.location.state(),
+    necesita_asistencia_movilidad: faker.datatype.boolean({ probability: 0.3 }),
+  },
+  {
+    nombre: faker.person.firstName(),
+    apellido: faker.person.lastName(),
+    cedula: faker.helpers.rangeToNumber({ min: 10000000, max: 99999999 }),
+    telefono: faker.phone.number('09# ## ## ##'),
+    departamento: faker.location.state(),
+    necesita_asistencia_movilidad: faker.datatype.boolean({ probability: 0.3 }),
+  },
+  {
+    nombre: faker.person.firstName(),
+    apellido: faker.person.lastName(),
+    cedula: faker.helpers.rangeToNumber({ min: 10000000, max: 99999999 }),
+    telefono: faker.phone.number('08# ### ###'),
+    departamento: faker.location.state(),
+    necesita_asistencia_movilidad: faker.datatype.boolean({ probability: 0.3 }),
+  },
+  {
+    nombre: faker.person.firstName(),
+    apellido: faker.person.lastName(),
+    cedula: faker.helpers.rangeToNumber({ min: 10000000, max: 99999999 }),
+    telefono: faker.phone.number('99# ### ###'),
+    departamento: faker.location.state(),
+    necesita_asistencia_movilidad: faker.datatype.boolean({ probability: 0.3 }),
+  },
+  {
+    nombre: '',
+    apellido: '',
+    cedula: 0,
+    telefono: '',
+    departamento: '',
+    necesita_asistencia_movilidad: true,
+  },
+  {
+    nombre: faker.person.firstName(),
+    apellido: faker.person.lastName(),
+    cedula: faker.helpers.rangeToNumber({ min: 10000000, max: 99999999 }),
+    telefono: faker.phone.number('09# ### ###'),
+    departamento: faker.location.state(),
+    necesita_asistencia_movilidad: false,
+  },
+  {
+    nombre: faker.person.firstName(),
+    apellido: faker.person.lastName(),
+    cedula: faker.helpers.rangeToNumber({ min: 10000000, max: 99999999 }),
+    telefono: faker.phone.number('09# ### ###'),
+    departamento: faker.location.state(),
+    necesita_asistencia_movilidad: true,
+  },
+  {
+    nombre: faker.person.firstName(),
+    apellido: faker.person.lastName(),
+    cedula: faker.helpers.rangeToNumber({ min: 10000000, max: 99999999 }),
+    telefono: faker.phone.number('09# ### ##'),
+    departamento: faker.location.state(),
+    necesita_asistencia_movilidad: faker.datatype.boolean({ probability: 0.3 }),
+  },
+  {
+    nombre: faker.person.firstName(),
+    apellido: faker.person.lastName(),
+    cedula: faker.helpers.rangeToNumber({ min: 10000000, max: 99999999 }),
+    telefono: faker.phone.number('09# ### ####'),
+    departamento: faker.location.state(),
+    necesita_asistencia_movilidad: faker.datatype.boolean({ probability: 0.3 }),
+  },
+]
+
+const sendPatient = async (patient: Person) => {
+    // const patient: Person = {
+    //   nombre: faker.person.firstName(),
+    //   apellido: faker.person.lastName(),
+    //   cedula: faker.helpers.rangeToNumber({ min: 10000000, max: 99999999 }),
+    //   telefono: faker.phone.number('09# ### ###'),
+    //   departamento: faker.location.state(),
+    //   necesita_asistencia_movilidad: faker.datatype.boolean({ probability: 0.3 }),
+    // };
 
     try {
       const response = await axios.post('http://localhost:3001/api/patient', patient);
@@ -31,7 +114,7 @@ const sendPatient = async () => {
     }
   };
   
-  sendPatient();
+testPatients.forEach(async (patient) => sendPatient(patient));
 
 // npm run build
 // npm start
--- Server/src/filters/filters.ts
+++ Server/src/filters/filters.ts
@@ -6,15 +6,13 @@ export const filtro1 = (input: Person) => {
     const primerosDosDigitos = telefono.slice(0, 2);
 
     if (telefono.length !== 9 || primerosDosDigitos !== '09') {
-        console.log(telefono, primerosDosDigitos)
-        console.log('El número de teléfono no tiene el formato correcto');
+        throw new Error('El número de teléfono no tiene el formato correcto');
     } else {
         console.log('El número de teléfono tiene el formato correcto')
     }
     return input;
 };
 
-// Segundo filtro: Convierte el input a mayúsculas.
 export const filtro4 = (input: Person) => {   
     
     if (input.necesita_asistencia_movilidad){
