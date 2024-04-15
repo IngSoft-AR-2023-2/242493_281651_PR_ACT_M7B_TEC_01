270a868786b52c0889f5c675ecaf6d97b116faa6
Daiana Aysa <da.aysa@gmail.com>
2024-04-15 08:43:07 -0300

    Add faker person

--- DataGenerator/src/index.ts
+++ DataGenerator/src/index.ts
@@ -1,12 +1,19 @@
 import axios from 'axios';
 import { faker } from '@faker-js/faker';
+import { Person } from '../../Interfaces/person';
 
-const sendWord = async () => {
-    const word = {
-      word: faker.word.words(1),
+const sendPatient = async () => {
+    const patient: Person = {
+      nombre: faker.person.firstName(),
+      apellido: faker.person.lastName(),
+      cedula: faker.helpers.rangeToNumber({ min: 10000000, max: 99999999 }),
+      telefono: faker.phone.number('09# ### ###'),
+      departamento: faker.location.state(),
+      necesita_asistencia_movilidad: faker.datatype.boolean({ probability: 0.3 }),
     };
+
     try {
-      const response = await axios.post('http://localhost:3001/api/word', word);
+      const response = await axios.post('http://localhost:3001/api/word', patient);
       console.log('Data sent successfully:', response.data);
     } catch (error) {
       if (axios.isAxiosError(error)) {
@@ -24,7 +31,7 @@ const sendWord = async () => {
     }
   };
   
-  sendWord();
+  sendPatient();
 
 // npm run build
 // npm start
---
+++ Interfaces/person.ts
@@ -0,0 +1,8 @@
+export interface Person {
+    nombre: string;
+    apellido: string;
+    cedula: number;
+    telefono: string;
+    departamento: string;
+    necesita_asistencia_movilidad: boolean;
+}
